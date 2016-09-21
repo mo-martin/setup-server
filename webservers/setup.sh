09ee6cf17813676d4a5dc4b1d0413c0bb8c41106
Moses Martin <moses.martin@skybettingandgaming.com>
2016-09-21 16:10:25 +0100

    Progress made on setting up server

---
+++ webservers/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt-get update -y
+# git
+sudo apt-get install git -y
+# nginx
+sudo apt-get install nginx -y
+# npm and nodejs
+sudo apt-get install npm -y
+sudo apt-get install nodejs -y
+# mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-org
+sudo tee -a /etc/systemd/system/mongodb.service <<EOF
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+
+[Service]
+User=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl start mongodb
+# 
