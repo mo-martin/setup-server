6148d4c2e2ee4646255361d6992ab86f66b9aae4
Moses Martin <moses.martin@skybettingandgaming.com>
2016-09-22 11:03:04 +0100

    Huge commit - shell script almost complete

--- webservers/setup.sh
+++ webservers/setup.sh
@@ -4,6 +4,7 @@ sudo apt-get update -y
 sudo apt-get install git -y
 # nginx
 sudo apt-get install nginx -y
+sudo cp ~/servers/webservers/default /etc/nginx/sites-enabled/default -f
 # npm and nodejs
 sudo apt-get install npm -y
 sudo apt-get install nodejs -y
@@ -12,17 +13,12 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update -y
 sudo apt-get install -y mongodb-org
-sudo tee -a /etc/systemd/system/mongodb.service <<EOF
-[Unit]
-Description=High-performance, schema-free document-oriented database
-After=network.target
-
-[Service]
-User=mongodb
-ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
-
-[Install]
-WantedBy=multi-user.target
-EOF
+sudo cp ~/servers/webservers/mongodb.service /etc/systemd/system/mongodb.service -f
 sudo systemctl start mongodb
-# 
+# pm2
+sudo npm install pm2 -y
+# Adding users and permissions
+sudo chown www-data:www-data /var/www -R
+sudo adduser --disabled-password --gecos "" production-user
+sudo adduser production-user www-data
+sudo chmod -R 775 /var/www
