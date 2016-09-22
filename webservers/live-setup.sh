3d4a49bc728c00b99357ed6d001d7d6468862056
Moses Martin <moses.martin@skybettingandgaming.com>
2016-09-22 16:46:49 +0100

    Server setup complete - tag added

--- webservers/live-setup.sh
+++ webservers/live-setup.sh
@@ -5,6 +5,7 @@ sudo apt-get install git -y
 # nginx
 sudo apt-get install nginx -y
 sudo cp ~/server-setup/webservers/default /etc/nginx/sites-enabled/default -f
+sudo service nginx restart
 # npm and nodejs
 sudo apt-get install npm -y
 sudo apt-get install nodejs -y
@@ -23,3 +24,4 @@ sudo chown www-data:www-data /var/www -R
 sudo adduser --disabled-password --gecos "" production-user
 sudo adduser production-user www-data
 sudo chmod -R 775 /var/www
+sudo rm /var/www/html/index*
