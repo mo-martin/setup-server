2ca67e1e288d3d0ffcf5c6e6593b27ac126aeafd
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-04 14:45:29 +0100

    docker config added

---
+++ docker/build-all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+eval "$(docker-machine env default)"
+
+set +e
+docker stop moses-app
+docker rm moses-app
+find my-node-api/api/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
+find my-node-app/app/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
+set -e
+
+rm -rf /tmp/node-api/api
+rm -rf /tmp/node-app/app
+mkdir -p /tmp/node-app/app
+mkdir -p /tmp/node-api/api
+
+cp -r ~/sparta/multi-vagrant-project/app/ /tmp/node-app/app
+cp -r ~/sparta/multi-vagrant-project/api/ /tmp/node-api/api
+rm -rf /tmp/node-app/app/.git
+rm -rf /tmp/node-app/app/.gitignore
+rm -rf /tmp/node-app/api/.git
+rm -rf /tmp/node-app/api/.gitignore
+
+cp -r /tmp/node-app/app/ ./my-node-app/app
+cp -r /tmp/node-api/api/ ./my-node-api/api
+
+pushd my-nodejs
+docker build --tag my-nodejs:latest .
+popd
+pushd my-node-app
+docker build --tag my-node-app:latest .
+popd
+pushd my-node-api
+docker build --tag my-node-api:latest .
+popd
+docker run --name moses-api -p 3001:3000 -d my-node-api:latest
+docker run --name moses-app -p 3000:3000 -d my-node-app:latest
+docker ps -a
